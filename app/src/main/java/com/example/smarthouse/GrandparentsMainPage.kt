f746522270531ab963466edb1aabc76a723c26e0
Rakhel Maria Linguraru <rakhel_maria@yahoo.com>
2021-01-10 16:50:11 +0200

    vocal assistant gif updated

--- app/src/main/java/com/example/smarthouse/GrandparentsMainPage.kt
+++ app/src/main/java/com/example/smarthouse/GrandparentsMainPage.kt
@@ -2,15 +2,13 @@ package com.example.smarthouse
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smarthouse.bathtub.BathtubMenuActivity
 import com.example.smarthouse.fridge.MenuFridgeActivity
-import com.example.smarthouse.security.SecurityMainActivity
+import com.example.smarthouse.light.LightActivity
 import com.example.smarthouse.temperature.TemperatureActivity
-import com.example.smarthouse.vacuum.VacuumOffActivity
 
 class GrandparentsMainPage: AppCompatActivity() {
 
--- app/src/main/java/com/example/smarthouse/KidsMainPage.kt
+++ app/src/main/java/com/example/smarthouse/KidsMainPage.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smarthouse.bathtub.BathtubMenuActivity
-import com.example.smarthouse.security.SecurityMainActivity
-import com.example.smarthouse.temperature.TemperatureActivity
-import com.example.smarthouse.vacuum.VacuumOffActivity
+import com.example.smarthouse.light.LightActivity
 
 class KidsMainPage : AppCompatActivity() {
 
